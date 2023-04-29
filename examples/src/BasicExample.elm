1120cb6f0697df44041d430d1724cf955d2ea722
agj <ale@agj.cl>
2023-04-29 14:48:42 -0400

    Add basic example

---
+++ examples/src/BasicExample.elm
@@ -0,0 +1,39 @@
+module PolygonExample exposing (main)
+
+import Browser
+import Html
+import Knob exposing (Knob)
+
+
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { knob : Knob Int }
+
+
+type Msg
+    = KnobUpdated (Knob Int)
+
+
+init =
+    { knob = Knob.int { step = 1, initial = 0 } }
+
+
+update msg model =
+    case msg of
+        KnobUpdated knobState ->
+            { knob = knobState }
+
+
+view model =
+    Html.div []
+        [ Html.text (String.fromInt (Knob.value model.knob))
+        , Knob.view KnobUpdated model.knob
+        , Knob.styles
+        ]
