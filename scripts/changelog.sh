020730137313d5696bde80d8339d4bfc06baf93f
agj <ale@agj.cl>
2023-06-21 14:30:06 -0400

    Listen to changelog changes to regenerate preview

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -5,7 +5,7 @@ trap 'exit' INT TERM ERR
 trap 'kill 0' EXIT
 
 # Convert changelog markdown to HTML.
-pandoc -f markdown -t html CHANGELOG.md > .temp/changelog/index.html
+(ls CHANGELOG.md | entr -s 'pandoc -f markdown -t html CHANGELOG.md > .temp/changelog/index.html ; echo Updated.') &
 
 # Serve the HTML to view in a browser.
 serve .temp/changelog/ -l 3000
