fb9949f023ca7376f545eae903d87271cc3782a5
agj <ale@agj.cl>
2023-06-21 19:18:36 -0400

    Improve changelog.sh

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# This script starts a server with a live-reloading HTML preview of the changelog.
+
 mkdir -p .temp/changelog/
 
 # Clean-up background processes on exit.
@@ -5,7 +9,7 @@ trap 'exit' INT TERM ERR
 trap 'kill 0' EXIT
 
 # Convert changelog file to HTML.
-(ls CHANGELOG.md | entr -s 'pandoc -f markdown -t html -s CHANGELOG.md > .temp/changelog/index.html') &
+find CHANGELOG.md | entr -s 'pandoc -f markdown -t html -s CHANGELOG.md > .temp/changelog/index.html' &
 
 # Serve the HTML to view in a browser, with live reloading.
 live-server --port=3000 .temp/changelog/
