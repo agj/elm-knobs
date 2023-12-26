97703fd847730bc3f102e84f0047c6dec01310b6
agj <ale@agj.cl>
2023-12-25 23:52:45 -0300

    Fix interactive docs index generation, and add "(latest)" text

--- scripts/build-interactive-docs-index.sh
+++ scripts/build-interactive-docs-index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source './scripts/functions.sh'
+source ./scripts/functions.sh
 
 outputDir=./interactive-docs/output
 
@@ -8,7 +8,20 @@ echo "Generating interactive docs version index."
 
 mkdir -p "$outputDir"
 
-versions=$(find "$outputDir" -maxdepth 1 -mindepth 1 -type d  | awk -F '/' '{print $NF}')
-versionLinks=$(echo "$versions" | sed -E 's|(.*)|<li><a href="\1">\1</a></li>|g')
+versions=$(
+  find "$outputDir" -maxdepth 1 -mindepth 1 -type d \
+  | awk -F '/' '{print $NF}' \
+  | sort --reverse
+)
+versionLinks=$(
+  echo "$versions" \
+  | awk '
+      NR == 1 { print "<li><a href=\"" $0 "\">" $0 " (latest)</a></li>" }
+      NR != 1 { print "<li><a href=\"" $0 "\">" $0 "</a></li>" }
+    ' \
+  | tr -d '\n'
+)
 
-sed -E "s|#versionLinks#|$versionLinks|" < ./interactive-docs/src/versions-index.html > "$outputDir/index.html"
+sed -E "s|#versionLinks#|$versionLinks|" \
+  < ./interactive-docs/src/versions-index.html \
+  > "$outputDir/index.html"
