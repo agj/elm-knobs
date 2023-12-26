7acc65bec71acf9b94e57930e72f7501fbc7abfd
agj <ale@agj.cl>
2023-12-25 22:52:42 -0300

    Fix check-versions won't catch some links to Github

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -5,16 +5,16 @@ getCurrentVersion() {
 }
 
 getAllVersionsInChangelog() {
-  grep -E '## \[[.[:digit:]]+\]' CHANGELOG.md | sed -E 's/^.*\[(.+)\].*$/\1/'
+  grep -E '## \[[0-9.]+\]' CHANGELOG.md | sed -E 's/^.*\[(.+)\].*$/\1/'
 }
 
 getAllVersionsInGit() {
-  git tag | grep -E '^[.[:digit:]]+$'
+  git tag | grep -E '^[0-9.]+$'
 }
 
 getVersionsUsedInLinks() {
   grep -E \
-    -e 'elm-knobs/[[:digit:].]+' \
+    -e 'elm-knobs/([a-z0-9]+/)?[0-9.]+' \
     "$@" \
-  | sed -E 's/^.*[/]([[:digit:].]+)[/].*$/\1/'
+  | sed -E 's/^.*[/]([0-9.]+)[/].*$/\1/'
 }
