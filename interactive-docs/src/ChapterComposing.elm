d87b8758f1106a675a01845500f4c7b1a5bf9509
agj <ale@agj.cl>
2023-12-30 15:28:12 -0300

    Progress writing the composition docs

--- interactive-docs/src/ChapterComposing.elm
+++ interactive-docs/src/ChapterComposing.elm
@@ -4,12 +4,13 @@ import ElmBook exposing (Msg)
 import ElmBook.Chapter exposing (Chapter)
 import Knob exposing (Knob)
 import KnobDoc exposing (KnobDoc)
+import String.Extra
 
 
 chapter =
     ElmBook.Chapter.chapter "Composition"
         |> ElmBook.Chapter.withStatefulComponentList
-            [ composeComponent ]
+            [ toComponent composeDoc ]
         |> ElmBook.Chapter.render content
 
 
@@ -18,8 +19,9 @@ content =
     """
 Composition.
 
-<component with-label="compose" />
+$compose$
 """
+        |> String.replace "$compose$" (toTemplate composeDoc)
 
 
 type alias Model =
@@ -28,20 +30,51 @@ type alias Model =
 
 
 init =
-    { compose = Knob.float { step = 1, initial = 1 }
+    { compose = composeDoc.init_
     }
 
 
 
--- Components
+-- Docs
 
 
-composeComponent =
+composeDoc =
+    { name = "compose"
+    , description = "Bla."
+    , init_ = Knob.float { step = 1, initial = 1 }
+    , code = "Knob.float { step = 1, initial = 1 }"
+    , get = \model -> model.compose
+    , set = \model knob -> { model | compose = knob }
+    , toString = String.fromFloat
+    }
+
+
+
+-- Utils
+
+
+toComponent =
     KnobDoc.toComponent
         .composing
         (\model sharedModel -> { sharedModel | composing = model })
-        { name = "compose"
-        , get = \model -> model.compose
-        , set = \model knob -> { model | compose = knob }
-        , toString = String.fromFloat
-        }
+
+
+toTemplate : { a | name : String, description : String, code : String } -> String
+toTemplate { name, description, code } =
+    """
+## $name$
+
+$description$
+
+<component
+    with-label="$name$"
+    with-hidden-label="true"
+/>
+
+```elm
+$code$
+```
+"""
+        |> String.replace "$name$" name
+        |> String.replace "$description$" description
+        |> String.replace "$code$" (code |> String.Extra.unindent |> String.trim)
