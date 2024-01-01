3dffc485e3779d63a8035a9da20f0b986bb2ec91
agj <ale@agj.cl>
2024-01-01 15:32:07 -0300

    Improve docs text

--- interactive-docs/src/ChapterIntroduction.elm
+++ interactive-docs/src/ChapterIntroduction.elm
@@ -6,17 +6,23 @@ import ElmBook.Chapter exposing (Chapter)
 
 
 chapter =
-    ElmBook.Chapter.chapter "Introduction"
+    ElmBook.Chapter.chapter "What this is"
         |> ElmBook.Chapter.render content
 
 
 content : String
 content =
     """
-This is the interactive documentation for the [**elm-knobs** v$elmKnobsVersion$
-Elm package][package]. Follow that link for a full explanation of the package
-and its functionality. This documentation only contains interactive examples of the
-“knobs” contained in it, so that you can see them in action in your browser.
+This is the interactive documentation for the [agj/**elm-knobs**
+$elmKnobsVersion$ Elm package][package]. This documentation contains interactive
+examples of the knobs, so that you can see them in action in your browser.
+Select a section from the menu on the left to jump to the examples.
+
+The package offers a way to easily tweak values interactively within a running
+Elm app, using a panel of input controls that map to those values, which we call
+“knobs” here. While not in use, they recede to an icon in the lower-left corner.
+For a full explanation of the package and its functionality, [see the API
+docs][package].
 
 [package]: $apiDocsUrl$
 """
