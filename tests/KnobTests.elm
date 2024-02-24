2b1c68b7082069213a790a6caeb879088a6e8453
agj <ale@agj.cl>
2024-02-23 21:42:04 -0300

    Extract repeating code

--- tests/KnobTests.elm
+++ tests/KnobTests.elm
@@ -3,19 +3,25 @@ module KnobTests exposing (..)
 import Html exposing (Html)
 import Knob exposing (Knob)
 import Test
-import Test.Html.Event as Event
+import Test.Html.Event as Event exposing (Event)
 import Test.Html.Query as Query
 import Test.Html.Selector as Selector
 
 
 int =
     Test.describe "int"
-        [ Test.test "bla" <|
+        [ Test.test "Can input" <|
             \() ->
                 Knob.int { step = 1, initial = 0 }
-                    |> Knob.view Knob.value
-                    |> Query.fromHtml
-                    |> Query.find [ Selector.tag "input" ]
-                    |> Event.simulate (Event.input "10")
+                    |> simulateInput "10"
                     |> Event.expect 10
         ]
+
+
+simulateInput : String -> Knob a -> Event a
+simulateInput inputString knob =
+    knob
+        |> Knob.view Knob.value
+        |> Query.fromHtml
+        |> Query.find [ Selector.tag "input" ]
+        |> Event.simulate (Event.input inputString)
